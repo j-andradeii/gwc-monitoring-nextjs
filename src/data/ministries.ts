3047a2cedf915f1cd129d14161be0fd82e770951
Joseph Andrade <joseph_andrade@outlook.ph>
2026-02-21 08:33:53 +0800

    feat: Update ministry titles for Couples and Kids Church, and add a specific description for Gateway Kids.

--- src/data/ministries.ts
+++ src/data/ministries.ts
@@ -39,15 +39,15 @@ export const ministries: Ministry[] = [
     },
     {
         id: '3',
-        title: 'Couples',
+        title: 'Gateway Couples',
         image: 'https://gtxngthtpisigkys.public.blob.vercel-storage.com/couple.jpg',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     },
     {
         id: '4',
-        title: 'Kids Church',
+        title: 'Gateway Kids',
         image: 'https://gtxngthtpisigkys.public.blob.vercel-storage.com/kids.jpg',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        description: 'Gateway Church is ready and happy to serve everyone who visits with their precious ones by ushering them to our Kids’ Corner. There, children play, sing songs of God’s goodness, share their stories, and create art. They also discover great stories from the Bible, learning that the same amazing things God did for His people then, He will do for them today. We are honored to support parents who desire to "train up a child in the way they should go" (Proverbs 22:6).',
     },
     {
         id: '5',
