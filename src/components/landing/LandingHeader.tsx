5a7b5ce0113d9bdd746616ec5af49e1c485dbfb7
Joseph Andrade <joseph_andrade@outlook.ph>
2026-06-02 11:00:04 +0800

    update gwwoth tab content

--- src/components/landing/LandingHeader.tsx
+++ src/components/landing/LandingHeader.tsx
@@ -127,11 +127,11 @@ export const LandingHeader: React.FC = () => {
                 Events
               </Link>
             </li>
-            {/* <li>
+            <li>
               <Link href="/connect" className={isActive('/connect') ? 'active' : ''} onClick={closeMobileMenu}>
                 Connect
               </Link>
-            </li> */}
+            </li>
             <li
               ref={giveRef}
               className={`nav-dropdown ${isGiveOpen ? 'dropdown-open' : ''}`}
